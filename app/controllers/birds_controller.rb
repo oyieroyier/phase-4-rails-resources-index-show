a94604abc57466777e8f061e2f902adcd518385c
Bob Oyier <boboyier92@gmail.com>
2023-03-14 22:23:04 +0300

    Done!

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,16 @@
+class BirdsController < ApplicationController
+  def index
+    birds = Bird.all
+    render json: birds
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+
+    if bird
+      render json: bird, status: :ok
+    else
+      render json: { error: 'Hakuna nyuni kama hiyo!!' }, status: :not_found
+    end
+  end
+end
